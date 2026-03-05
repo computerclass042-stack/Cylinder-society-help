064b2e07714147f3aac183bff851fc369052ffc8
computerclass042-stack <computerclass042@gmail.com>
2026-03-05 14:50:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,104 @@
-import streamlit as st
-from streamlit_gsheets import GSheetsConnection
-import pandas as pd
-
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-data = conn.read()
-
-st.dataframe(data)
+import random
+import csv
+from datetime import datetime, timedelta
 
 price_per_cylinder = 900
+total_stock = 100
+total_sales = 0
+admin_password = "1234"
 
-st.title("🧯 Society Gas Cylinder System")
-st.subheader("📋 New Cylinder Booking")
+while True:
+    print("\n====== Gas Cylinder System ======")
+    print("1. Customer Booking")
+    print("2. Admin Login")
+    print("3. Exit")
 
-name = st.text_input("Customer Name")
-phone = st.text_input("Phone Number")
-address = st.text_area("Delivery Address")
-quantity = st.number_input("Quantity", min_value=1, step=1)
+    main_choice = input("Enter your choice: ")
 
-if st.button("Generate OTP & Book"):
+    # ================= CUSTOMER PANEL =================
+    if main_choice == "1":
 
-    booking_date = datetime.now()
-    delivery_date = booking_date + timedelta(days=3)
+        if total_stock <= 0:
+            print("Sorry! No Stock Available.")
+            continue
 
-    total_amount = quantity * price_per_cylinder
-    otp = random.randint(1000,9999)
+        print("\n--- Customer Booking ---")
 
-    st.success("Booking Successful ✅")
+        name = input("Enter Customer Name: ")
+        address = input("Enter Address: ")
+        phone = input("Enter Phone Number: ")
+        quantity = int(input("Enter Quantity: "))
 
-    st.write("Delivery Date:", delivery_date.strftime("%d-%m-%Y"))
-    st.write("Total Bill: ₹", total_amount)
-    st.write("OTP:", otp)
+        if quantity > total_stock:
+            print("Not enough stock available!")
+            continue
 
+        booking_date = datetime.now()
+        delivery_date = booking_date + timedelta(days=3)
 
+        total_amount = quantity * price_per_cylinder
+        otp = random.randint(1000, 9999)
+
+        total_stock -= quantity
+        total_sales += total_amount
+
+        with open("bookings.csv", "a", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerow([
+                name,
+                address,
+                phone,
+                quantity,
+                delivery_date.strftime("%d-%m-%Y"),
+                total_amount,
+                otp
+            ])
+
+        print("\n✅ Booking Successful!")
+        print("Delivery Date:", delivery_date.strftime("%d-%m-%Y"))
+        print("Total Bill: ₹", total_amount)
+        print("OTP sent to customer:", otp)
+
+        print("\n--- Delivery Verification ---")
+        entered_otp = int(input("Delivery Boy Enter OTP(Entre OTP WHEN DELIVERY IS DONE): "))
+
+        if entered_otp == otp:
+            print("Delivery Confirmed ✅")
+        else:
+            print("Invalid OTP ❌ Delivery Failed")
+
+    # ================= ADMIN PANEL =================
+    elif main_choice == "2":
+        password = input("Enter Admin Password: ")
+
+        if password == admin_password:
+
+            while True:
+                print("\n--- Admin Panel ---")
+                print("1. Show Stock")
+                print("2. Show Total Sales")
+                print("3. Back")
+
+                admin_choice = input("Enter choice: ")
+
+                if admin_choice == "1":
+                    print("Available Stock:", total_stock)
+
+                elif admin_choice == "2":
+                    print("Total Sales: ₹", total_sales)
+
+                elif admin_choice == "3":
+                    break
+
+                else:
+                    print("Invalid Choice")
+
+        else:
+            print("Wrong Password ❌")
+
+    elif main_choice == "3":
+        print("Thank You!")
+        break
+
+    else:
+        print("Invalid Choice!")
